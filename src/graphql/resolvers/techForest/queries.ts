31a2e665b7510f0acbaabaa76020ab16e1206938
kosuke-17 <tamutamu17@icloud.com>
2022-02-25 22:33:03 +0900

    コメントアウト追加

--- src/graphql/resolvers/techForest/queries.ts
+++ src/graphql/resolvers/techForest/queries.ts
@@ -1,7 +1,14 @@
 import { TechLeaf } from "../../../models/TechForest.model";
 
 const techForestQueries = {
-  getAllTechLeaf: async (_parent: any, args: any) => {
+  /**
+   * techLeaf一覧情報を取得する.
+   *
+   * @param _parent - 親リゾルバから受け取るオブジェクト
+   * @param args - 受け取ってきた引数
+   * @returns - techLeaf一覧情報
+   */
+  getAllTechLeaf: async (_parent: any, _args: any) => {
     return TechLeaf.find({});
   },
 };
