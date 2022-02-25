2273e53edd1d3760fc673704c51d512ee0edeecd
kosuke-17 <tamutamu17@icloud.com>
2022-02-25 22:34:06 +0900

    ユーザー一覧を取得する

--- src/graphql/resolvers/user/queries.ts
+++ src/graphql/resolvers/user/queries.ts
@@ -1,5 +1,21 @@
+import { User } from "../../../models/User.model";
+
 const userQueries = {
-  user: async (_: any, args: any) => {},
+  /**
+   * ユーザー一覧情報を取得する.
+   *
+   * @param _parent - 親リゾルバから受け取るオブジェクト
+   * @param _args - 受け取ってきた引数
+   * @returns - ユーザー一覧情報
+   */
+  getAllUser: async (_parent: any, _args: any) => {
+    return User.find({});
+  },
+  user: async (_parent: any, { id }: any) => {
+    const objId = id;
+
+    return User.findById({ objId });
+  },
 };
 
 export default userQueries;
