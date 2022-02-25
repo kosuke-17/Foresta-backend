2308da4a79becf47010d896ac8b1f020392b7d37
kosuke-17 <tamutamu17@icloud.com>
2022-02-25 15:35:48 +0900

    コメントアウト作成

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
+// envファイルの利用を可能にする
 dotenv.config();
-
+// 設定されている環境変数にPORT番号がなければ4000版で起動
 const port = process.env.PORT || 4000;
-
 // プロジェクトの開発環境を判定
 const env = {
   development: process.env.NODE_ENV === "development",
@@ -11,7 +11,7 @@ const env = {
   staging: process.env.NODE_ENV === "staging",
   production: process.env.NODE_ENV === "production",
 };
-
 export { port, env };
 
+// MongoDBのURLを取得
 export const MONGOURI = process.env.MONGO_URI || "";
