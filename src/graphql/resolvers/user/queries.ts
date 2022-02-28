5833dd6d4b27e13c0e7ea48cd3699d36eb08a3c6
kosuke-17 <tamutamu17@icloud.com>
2022-02-28 13:30:23 +0900

    todo一覧情報を取得

--- src/graphql/resolvers/user/queries.ts
+++ src/graphql/resolvers/user/queries.ts
@@ -1,16 +1,25 @@
-import { User } from "../../../models/User.model";
+import { User, UserTodo } from "../../../models/User.model";
 
 const userQueries = {
   /**
    * ユーザー一覧情報を取得する.
    *
-   * @returns - ユーザー一覧情報
+   * @returns ユーザー一覧情報
    */
   getAllUser: async (_parent: any, _args: any) => {
-    return User.find({});
+    return await User.find({});
   },
   user: async (_parent: any, { _id }: any) => {
-    return User.findById({ _id });
+    return await User.findById({ _id });
+  },
+  /**
+   * ユーザーのTodo一覧情報を取得する
+   *
+   * @param userId - ユーザーID
+   * @returns todo一覧情報
+   */
+  getAllTodoByUser: async (_parent: any, { userId }: any) => {
+    return await UserTodo.find({ userId: userId });
   },
 };
 
